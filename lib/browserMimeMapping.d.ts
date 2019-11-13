17bc9e6d85d001798174eeaa41b5c7c88cf78a22
周伟濠 <zhouweihao@cvte.com>
2019-11-13 22:12:13 +0800

    编译后的lib文件

---
+++ lib/browserMimeMapping.d.ts
@@ -0,0 +1,14 @@
+declare const _default: {
+    OGG: string[];
+    AAC: string[];
+    MP3: string[];
+    WAV: string[];
+    AVI: string[];
+    WMA: string[];
+    RMVB: string[];
+    FLV: string[];
+    MP4: string[];
+    JPG: string[];
+    PNG: string[];
+};
+export default _default;
