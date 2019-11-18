211113450ae5dba6814bcf41aec1fe002a6fab7d
周伟濠 <zhouweihao@cvte.com>
2019-11-18 10:34:39 +0800

    remove lib

--- lib/util.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function isInBrowser(): boolean;
-export declare function isObject(arg: any): boolean;
-export declare function isObjectObject(arg: any): boolean;
-export declare function isString(arg: any): boolean;
-export declare function isStringArray(arg: any): boolean;
-export declare function isFunction(arg: any): boolean;
-export declare function isFileInstance(arg: any): boolean;
-export declare function isUint8Array(arg: any): boolean;
-export declare function getFileExt(file: File): string | null;
